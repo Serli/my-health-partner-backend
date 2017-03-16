82803dc5cf7385ef6b4035e42fbfd7751522d94f
salquier <simonalquier92@gmail.com>
2017-03-16 14:44:25 +0100

    Update performance test in order to test the utility of each feature

--- spring/src/test/scala/model/ModelPerformanceTest.scala
+++ spring/src/test/scala/model/ModelPerformanceTest.scala
@@ -9,70 +9,161 @@ import org.scalatest._
 
 class ModelPerformanceTest extends FunSuite with Matchers with BeforeAndAfterAll{
 
-  var data : RDD[LabeledPoint] = _
+  var datas : List[RDD[LabeledPoint]] = List()
 
   override protected def beforeAll(): Unit = {
     val sc = SparkContextLoader.sc
     val cassandraRDD = sc.cassandraTable("accelerometerdata", "feature")
-    data = cassandraRDD.select("activity", "mean_x", "mean_y", "mean_z", "variance_x", "variance_y", "variance_z", "avg_abs_diff_x", "avg_abs_diff_y", "avg_abs_diff_z", "resultant", "avg_time_peak")
-      .map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+    val cassandraRow = cassandraRDD.select("activity", "mean_x", "mean_y", "mean_z", "variance_x", "variance_y", "variance_z", "avg_abs_diff_x", "avg_abs_diff_y", "avg_abs_diff_z", "resultant", "avg_time_peak")
+////////// All /////////////
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
       .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
-      .cache()
+////////// - 1 //////////////
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+////////// - 2 //////////////
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+////////// - 3 //////////////
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("resultant"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z"), entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+////////// - 4 //////////////
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("avg_time_peak")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("resultant")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("avg_abs_diff_x"), entry.getDouble("avg_abs_diff_y"), entry.getDouble("avg_abs_diff_z")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("variance_x"), entry.getDouble("variance_y"), entry.getDouble("variance_z")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
+
+    datas :+= cassandraRow.map(entry => (entry.getLong("activity"), Vectors.dense(Array(entry.getDouble("mean_x"), entry.getDouble("mean_y"), entry.getDouble("mean_z")))))
+      .map(entry => CreateModel.getLabeledPoint(entry._1, entry._2))
 
   }
 
   test("RandomForestsTest") {
-    var error : Double = 0
-    var errorMax : Double = 0
-    var errorMin : Double = 100
-    for(_ <- 1 to 25) {
-      val splits = data.randomSplit(Array(0.5, 0.5))
-      val training = splits(0)
-      val test = splits(1)
-      val model = new RandomForests(training).createModel()
-      var e = test.map(p => (model.predict(p.features), p.label))
-                   .filter(p => p._1 != p._2)
-                   .count()
-                   .toDouble
-      e /= test.count().toDouble
-      if(e>errorMax) errorMax = e
-      if(e<errorMin) errorMin = e
-      error += e
+    for(data <- datas) {
+      var error: Double = 0
+      var errorMax: Double = 0
+      var errorMin: Double = 100
+      for (_ <- 1 to 25) {
+        val splits = data.randomSplit(Array(0.5, 0.5))
+        val training = splits(0)
+        val test = splits(1)
+        val model = new RandomForests(training).createModel()
+        var e = test.map(p => (model.predict(p.features), p.label))
+          .filter(p => p._1 != p._2)
+          .count()
+          .toDouble
+        e /= test.count().toDouble
+        if (e > errorMax) errorMax = e
+        if (e < errorMin) errorMin = e
+        error += e
+      }
+      error /= 25
+      println("Sample size : " + data.count)
+      println("Average error : " + error * 100 + "%")
+      println("Minimal error : " + errorMin * 100 + "%")
+      println("Maximal error : " + errorMax * 100 + "%")
+      // assert(error < 0.1, "Average error should be < 10%")
+      // assert(errorMax < 0.15, "Maximal error should be < 15%")
     }
-    error /= 25
-    println("Sample size : " + data.count)
-    println("Average error : " + error*100 + "%")
-    println("Minimal error : " + errorMin * 100 + "%")
-    println("Maximal error : " + errorMax * 100 + "%")
-    assert(error < 0.1, "Average error should be < 10%")
-    assert(errorMax < 0.15, "Maximal error should be < 15%")
   }
 
   test("DecisionTreesTest") {
-    var error : Double = 0
-    var errorMax : Double = 0
-    var errorMin : Double = 100
-    for(_ <- 1 to 25) {
-      val splits = data.randomSplit(Array(0.5, 0.5))
-      val training = splits(0)
-      val test = splits(1)
-      val model = new DecisionTrees(training).createModel()
-      var e = test.map(p => (model.predict(p.features), p.label))
-        .filter(p => p._1 != p._2)
-        .count()
-        .toDouble
-      e /= test.count().toDouble
-      if(e>errorMax) errorMax = e
-      if(e<errorMin) errorMin = e
-      error += e
+    for (data <- datas) {
+      var error: Double = 0
+      var errorMax: Double = 0
+      var errorMin: Double = 100
+      for (_ <- 1 to 25) {
+        val splits = data.randomSplit(Array(0.5, 0.5))
+        val training = splits(0)
+        val test = splits(1)
+        val model = new DecisionTrees(training).createModel()
+        var e = test.map(p => (model.predict(p.features), p.label))
+          .filter(p => p._1 != p._2)
+          .count()
+          .toDouble
+        e /= test.count().toDouble
+        if (e > errorMax) errorMax = e
+        if (e < errorMin) errorMin = e
+        error += e
+      }
+      error /= 25
+      println("Sample size : " + data.count)
+      println("Average error : " + error * 100.0 + "%")
+      println("Minimal error : " + errorMin * 100 + "%")
+      println("Maximal error : " + errorMax * 100 + "%")
+      // assert(error < 0.1, "Average error should be < 10%")
+      // assert(errorMax < 0.15, "Maximal error should be < 15%")
     }
-    error /= 25
-    println("Sample size : " + data.count)
-    println("Average error : " + error*100.0 + "%")
-    println("Minimal error : " + errorMin * 100 + "%")
-    println("Maximal error : " + errorMax * 100 + "%")
-    assert(error < 0.1, "Average error should be < 10%")
-    assert(errorMax < 0.15, "Maximal error should be < 15%")
   }
 
 }
